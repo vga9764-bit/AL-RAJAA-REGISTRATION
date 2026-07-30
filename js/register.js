df58c4be084daeb588bfd0cca8dc1d6dc6271d00
vga9764-bit <vga9764@gmail.com>
2026-07-30 21:36:25 +0300

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,37 @@
+import { db } from "./firebase.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
+const form = document.getElementById("playerForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const player = {
+    name: document.getElementById("name").value,
+    birth: document.getElementById("birth").value,
+    phone: document.getElementById("phone").value,
+    address: document.getElementById("address").value,
+    position: document.getElementById("position").value,
+    height: document.getElementById("height").value,
+    weight: document.getElementById("weight").value,
+    foot: document.getElementById("foot").value,
+    clubs: document.getElementById("clubs").value,
+    notes: document.getElementById("notes").value,
+    createdAt: serverTimestamp()
+  };
+
+  try {
+    await addDoc(collection(db, "players"), player);
+
+    alert("تم إرسال طلب التسجيل بنجاح");
+
+    form.reset();
+
+  } catch (error) {
+    alert("حدث خطأ: " + error.message);
+  }
+});
